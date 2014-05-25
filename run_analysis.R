915685ba110ffb052f03cf618b90980fa3934bed
githubprogammerx <petertimusk@programdocumentation.com>
2014-05-25 11:41:07 -0400

    here is the start of the project

---
+++ run_analysis.R
@@ -0,0 +1,28 @@
+##run_analysis
+body_acc_x_test <- read.delim("C:/Users/ptimusk/Documents/GitHub/courseraDataScience/GettingandCleaningData/CourseProject/UCI HAR Dataset/test/Inertial Signals/body_acc_x_test.txt", sep="" , header=FALSE)
+body_acc_y_test <- read.delim("C:/Users/ptimusk/Documents/GitHub/courseraDataScience/GettingandCleaningData/CourseProject/UCI HAR Dataset/test/Inertial Signals/body_acc_y_test.txt", sep="" , header=FALSE)
+body_acc_z_test <- read.delim("C:/Users/ptimusk/Documents/GitHub/courseraDataScience/GettingandCleaningData/CourseProject/UCI HAR Dataset/test/Inertial Signals/body_acc_z_test.txt", sep="" , header=FALSE)
+
+
+body_gyro_x_test <- read.delim("C:/Users/ptimusk/Documents/GitHub/courseraDataScience/GettingandCleaningData/CourseProject/UCI HAR Dataset/test/Inertial Signals/body_gyro_x_test.txt", sep="" , header=FALSE)
+body_gyro_y_test <- read.delim("C:/Users/ptimusk/Documents/GitHub/courseraDataScience/GettingandCleaningData/CourseProject/UCI HAR Dataset/test/Inertial Signals/body_gyro_y_test.txt", sep="" , header=FALSE)
+body_gyro_x_test <- read.delim("C:/Users/ptimusk/Documents/GitHub/courseraDataScience/GettingandCleaningData/CourseProject/UCI HAR Dataset/test/Inertial Signals/body_gyro_z_test.txt", sep="" , header=FALSE)
+
+
+total_acc_x_test <- read.delim("C:/Users/ptimusk/Documents/GitHub/courseraDataScience/GettingandCleaningData/CourseProject/UCI HAR Dataset/test/Inertial Signals/total_acc_x_test.txt", sep="" , header=FALSE)
+total_acc_y_test <- read.delim("C:/Users/ptimusk/Documents/GitHub/courseraDataScience/GettingandCleaningData/CourseProject/UCI HAR Dataset/test/Inertial Signals/total_acc_y_test.txt", sep="" , header=FALSE)
+total_acc_z_test <- read.delim("C:/Users/ptimusk/Documents/GitHub/courseraDataScience/GettingandCleaningData/CourseProject/UCI HAR Dataset/test/Inertial Signals/total_acc_z_test.txt", sep="" , header=FALSE)
+
+body_acc_x_train <- read.delim("C:/Users/ptimusk/Documents/GitHub/courseraDataScience/GettingandCleaningData/CourseProject/UCI HAR Dataset/train/Inertial Signals/body_acc_x_train.txt", sep="" , header=FALSE)
+body_acc_y_train <- read.delim("C:/Users/ptimusk/Documents/GitHub/courseraDataScience/GettingandCleaningData/CourseProject/UCI HAR Dataset/train/Inertial Signals/body_acc_y_train.txt", sep="" , header=FALSE)
+body_acc_z_train <- read.delim("C:/Users/ptimusk/Documents/GitHub/courseraDataScience/GettingandCleaningData/CourseProject/UCI HAR Dataset/train/Inertial Signals/body_acc_z_train.txt", sep="" , header=FALSE)
+
+
+body_gyro_x_train <- read.delim("C:/Users/ptimusk/Documents/GitHub/courseraDataScience/GettingandCleaningData/CourseProject/UCI HAR Dataset/train/Inertial Signals/body_gyro_x_train.txt", sep="" , header=FALSE)
+body_gyro_y_train <- read.delim("C:/Users/ptimusk/Documents/GitHub/courseraDataScience/GettingandCleaningData/CourseProject/UCI HAR Dataset/train/Inertial Signals/body_gyro_y_train.txt", sep="" , header=FALSE)
+body_gyro_x_train <- read.delim("C:/Users/ptimusk/Documents/GitHub/courseraDataScience/GettingandCleaningData/CourseProject/UCI HAR Dataset/train/Inertial Signals/body_gyro_z_train.txt", sep="" , header=FALSE)
+
+
+total_acc_x_train <- read.delim("C:/Users/ptimusk/Documents/GitHub/courseraDataScience/GettingandCleaningData/CourseProject/UCI HAR Dataset/train/Inertial Signals/total_acc_x_train.txt", sep="" , header=FALSE)
+total_acc_y_train <- read.delim("C:/Users/ptimusk/Documents/GitHub/courseraDataScience/GettingandCleaningData/CourseProject/UCI HAR Dataset/train/Inertial Signals/total_acc_y_train.txt", sep="" , header=FALSE)
+total_acc_z_train <- read.delim("C:/Users/ptimusk/Documents/GitHub/courseraDataScience/GettingandCleaningData/CourseProject/UCI HAR Dataset/train/Inertial Signals/total_acc_z_train.txt", sep="" , header=FALSE)
